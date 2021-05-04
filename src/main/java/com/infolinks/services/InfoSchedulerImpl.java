e9d508ddbfa244f8a6536242b8fbe1a9ddecb3fb
Eliezer <reuvene@mprest.com>
2021-05-04 09:16:45 +0300

    fix read @value from app.config

--- src/main/java/com/infolinks/services/InfoSchedulerImpl.java
+++ src/main/java/com/infolinks/services/InfoSchedulerImpl.java
@@ -3,15 +3,11 @@ package com.infolinks.services;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
+import javax.annotation.PostConstruct;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -19,9 +15,16 @@ public class InfoSchedulerImpl implements InfoScheduler {
 
     private final static Logger logger = LogManager.getLogger(CrawlerServiceImpl.class);
 
-    //    @Value("${scheduler.thread.pool.size}")
-//    private Integer schedulerPoolSize=50;
-    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(50);
+    private ScheduledExecutorService executorService;
+
+    @Value("${scheduler.thread.pool.size}")
+    private Integer schedulerPoolSize;
+
+
+    @PostConstruct
+    private void init() {
+        executorService = Executors.newScheduledThreadPool(schedulerPoolSize);
+    }
 
     @Override
     public void scheduleNow(Runnable runnable) {
